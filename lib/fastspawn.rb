801e2d991c767c76c1d77b746c0685889419a3bf
Ryan Tomayko <rtomayko@gmail.com>
2011-02-26 23:04:41 -0800

    fspawn uses normal fork + exec but is otherwise identical to vspawn

--- lib/fastspawn.rb
+++ lib/fastspawn.rb
@@ -4,6 +4,26 @@ module FastSpawn
 
   # fail fast when extension methods already defined due to twice-loading
   raise LoadError, "fastspawn extension already loaded" if method_defined?(:vspawn)
+
+  # Spawn a child process using fast vfork + exec.
+  #
+  # argv - Array of command line arguments passed to exec.
+  #
+  # Returns the pid of the newly spawned process.
+  # Raises NotImplemented when vfork is not supported on the current platform.
+  def vspawn(*argv)
+    raise NotImplemented
+  end
+
+  # Spawn a child process using a normal fork + exec.
+  #
+  # Returns the pid of the newly spawned process.
+  def fspawn(*argv)
+    fork do
+      exec(*argv)
+      exit! 1
+    end
+  end
 end
 
 # fastspawn extension methods replace ruby versions
--- test/test_fastspawn.rb
+++ test/test_fastspawn.rb
@@ -5,16 +5,14 @@ require 'test/unit'
 require 'fastspawn'
 
 class FastSpawnTest < Test::Unit::TestCase
-  def test_fastspawn_module_defined
-    assert defined?(FastSpawn)
-  end
+  include FastSpawn
 
   def test_fastspawn_methods_exposed_at_module_level
     assert FastSpawn.respond_to?(:vspawn)
   end
 
   def test_vspawn_simple
-    pid = FastSpawn.vspawn('true')
+    pid = vspawn('true')
     assert pid > 0
 
     chpid, status = Process.wait2
@@ -23,7 +21,16 @@ class FastSpawnTest < Test::Unit::TestCase
   end
 
   def test_vspawn_with_argv
-    pid = FastSpawn.vspawn('echo', 'some', 'stuff')
+    pid = vspawn('true', 'with', 'some arguments')
+    assert pid > 0
+
+    chpid, status = Process.wait2
+    assert_equal chpid, pid
+    assert_equal 0, status.exitstatus
+  end
+
+  def test_fspawn
+    pid = fspawn('true', 'with', 'some stuff')
     assert pid > 0
 
     chpid, status = Process.wait2
