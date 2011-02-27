e486354200c770599635d8a366201d940187f835
Ryan Tomayko <rtomayko@gmail.com>
2011-02-26 22:58:31 -0800

    extension methods replace Ruby versions

--- lib/fastspawn.rb
+++ lib/fastspawn.rb
@@ -1,6 +1,10 @@
-require 'fastspawn.so'
-
 module FastSpawn
-  extend self
   VERSION = '0.1'
+  extend self
+
+  # fail fast when extension methods already defined due to twice-loading
+  raise LoadError, "fastspawn extension already loaded" if method_defined?(:vspawn)
 end
+
+# fastspawn extension methods replace ruby versions
+require 'fastspawn.so'
