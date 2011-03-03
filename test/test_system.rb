0e74c932ee807dde304ff4bc061a3042046d582c
Aman Gupta <aman@tmm1.net>
2011-03-02 17:31:28 -0800

    fixup system and `

--- test/test_system.rb
+++ test/test_system.rb
@@ -5,19 +5,25 @@ class SystemTest < Test::Unit::TestCase
   include POSIX::Spawn
 
   def test_system
-    ret = system("exit")
+    ret = system("true")
     assert_equal ret, true
     assert_equal $?.exitstatus, 0
   end
 
   def test_system_nonzero
-    ret = system("exit 1")
+    ret = system("false")
+    assert_equal ret, false
+    assert_equal $?.exitstatus, 1
+  end
+
+  def test_system_nonzero_via_sh
+    ret = system("sh -c 'exit 1'")
     assert_equal ret, false
     assert_equal $?.exitstatus, 1
   end
 
   def test_system_failure
-    ret = system("nosuch 2> /dev/null")
+    ret = system("nosuch")
     assert_equal ret, false
   end
 end
